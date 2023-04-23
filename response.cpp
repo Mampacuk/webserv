2bb9b97a570ea404142ffa100049c891993cfeb4
Anahit Apresyan <aapresya@c1r4s6.42yerevan.am>
2023-04-23 16:49:17 +0400

    to_string changed to construct response

--- response.cpp
+++ response.cpp
@@ -4,9 +4,12 @@ namespace ft
 {
 
 	// response::response(): _status(), _body(), _headers(), _req(NULL) {}
-	response::response(const response &other): _status(other._status), _body(other._body), _headers(other._headers), _req(other._req) {}
+	response::response(const response &other): _status(other._status), _body(other._body), _headers(other._headers), _response(other._response), _req(other._req) {}
 
-	response::response(const request &request, int status_code): _status(status_code), _body(), _headers(), _req(request) {}
+	response::response(const request &request, int status_code): _status(status_code), _body(), _headers(), _response(other._response), _req(request)
+	{
+		generate_response();
+	}
 
 	response::~response() {}
 
@@ -24,19 +27,34 @@ namespace ft
 		return (_req._socket);
 	}
 
-	std::string response::to_string() const
+	void response::generate_response()
 	{
-		std::string response = "HTTP/1.1 ";
+		if (is_error_code(_status))
+			find_error_page();
+
+	}
+
+	void find_error_page()
+	{
+
+	}
+
+	bool response::is_error_code(int status_code)
+	{
+		return (status_code >= 400);
+	}
+
+	void response::construct_response()
+	{
+		_response = "HTTP/1.1 ";
 		std::stringstream s;
 		
 		s << _status;
 
-		response += s.str() + " " + status_to_string(_status) + CRLF;
+		_response += s.str() + " " + status_to_string(_status) + CRLF;
 		for (string_map::const_iterator it = _headers.begin(); it != _headers.end(); it++)
-			response += it->first + ": " + it->second + CRLF;
-		response += CRLF + _body;
-	
-		return (response);
+			_response += it->first + ": " + it->second + CRLF;
+		_response += CRLF + _body;
 	}
 
 	std::string response::status_to_string(int status_code) const
--- response.hpp
+++ response.hpp
@@ -13,15 +13,19 @@ namespace ft
 			string_map	_headers;
 			std::string _body;
 			const 		request &_req;
+			std::string _response;
 			// response();
 			response &operator=(const response &other);
-		public:
 			response(const response &other);
+		public:
 			response(const request &request, int status_code = HTTP_STATUS_OK);
 			~response();
 			operator int() const;
-			std::string to_string() const;
 		private:
+			void construct_response();
+			void generate_response();
+			void find_error_page();
+			bool is_error_code(int status_code);
 			std::string status_to_string(int status_code) const;
 	};
 }
